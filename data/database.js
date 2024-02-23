d0de35c231d40000391aa6ba6332ee22d01a8615
ArnavU <arnavumarkar3@gmail.com>
2024-02-24 04:18:28 +0530

    mongodb cloud connected

--- data/database.js
+++ data/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = () => {
 	mongoose
-		.connect('mongodb://127.0.0.1:27017/', {
+		.connect(process.env.MONGO_URI, {
 			dbName: "collegesCutoff",
 		})
 		.then((c) => console.log(`Database Connected with ${c.connection.host}`))
