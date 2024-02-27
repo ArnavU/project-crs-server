28f893170270a4392299e1ea981008227b0b74dd
ArnavU <arnavumarkar3@gmail.com>
2024-02-27 23:02:02 +0530

    get pdf year by year

--- controllers/getAllPdfs.js
+++ controllers/getAllPdfs.js
@@ -1,9 +1,12 @@
 import { PdfModel } from "../models/pdf.js";
 
 export const getAllPdfs = async (req, res) => {
+	const {year} = req.params;
+	console.log(year)
 	try {
 		// Retrieve all PDFs from the database
-		const pdfs = await PdfModel.find();
+		const pdfs = await PdfModel.find({year: year});
+		console.log("Data fetched")
 
 		// Send the array of PDFs in the response
 		res.status(200).json(pdfs);
