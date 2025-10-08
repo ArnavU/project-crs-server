190a4268ec72d72e7f1258a6075ab2d7e691e82b
ArnavU <arnavumarkar.intern@peopleprudent.co.in>
2025-10-08 18:59:55 +0530

    add / route for api call by cron-job.org

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ cron.schedule('* * * * *', () => {
   console.log('Running an empty task every minute'); // to keep the server alive
 });
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the API CRS(College Recommendation System). Made with love by Arnav Umarkar.');
+});
+
 app.listen(process.env.PORT, ()=> {
     console.log(`Server is running at port ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
 })
