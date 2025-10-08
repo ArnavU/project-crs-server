9f966b5fb6ce3e921fe533ca20019208f0ccb6dd
ArnavU <arnavumarkar.intern@peopleprudent.co.in>
2025-10-08 18:40:36 +0530

    add node cron-job to keep the server alive

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 import { app } from "./app.js"; 
 import { connectDB } from "./data/database.js";
+import cron from 'node-cron';
 
 connectDB();
+
+cron.schedule('* * * * *', () => {
+  console.log('Running an empty task every minute'); // to keep the server alive
+});
 
 app.listen(process.env.PORT, ()=> {
     console.log(`Server is running at port ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
